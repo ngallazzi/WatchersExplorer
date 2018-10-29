19e55427f725b7a299ada246bf18c2761bde09bf
Nicola Gallazzi <nicola.gallazzi.dev@gmail.com>
2018-10-29 18:02:08 +0100

    rough search implementation

---
+++ app/src/main/java/com/ngallazzi/watchersexplorer/RepositoryAdapter.kt
@@ -0,0 +1,39 @@
+package com.ngallazzi.watchersexplorer
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ngallazzi.watchersexplorer.R.id.ivOwnerImage
+import com.ngallazzi.watchersexplorer.remote.models.Repository
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_repository.view.*
+
+/**
+ * WatchersExplorer
+ * Created by Nicola on 10/29/2018.
+ * Copyright © 2018 Zehus. All rights reserved.
+ */
+class RepositoryAdapter(private val repositories: ArrayList<Repository>, val context: Context) :
+        RecyclerView.Adapter<ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_repository, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tvRepositoryName.text = repositories[position].name
+        Picasso.get().load(repositories[position].owner.avatarUrl).into(holder.ivOwnerImage)
+    }
+
+    // Gets the number of repositories in the list
+    override fun getItemCount(): Int {
+        return repositories.size
+    }
+}
+
+class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    // Holds the TextView that will add each animal to
+    val tvRepositoryName = view.tvRepositoryName!!
+    val ivOwnerImage = view.ivOwnerImage
+}
--- app/src/main/java/com/ngallazzi/watchersexplorer/SearchRepositoriesActivity.kt
+++ app/src/main/java/com/ngallazzi/watchersexplorer/SearchRepositoriesActivity.kt
@@ -3,23 +3,27 @@ package com.ngallazzi.watchersexplorer
 import android.R.attr.duration
 import android.app.SearchManager
 import android.content.Context
-import android.content.Intent
-import android.content.Intent.*
+import android.content.Intent.ACTION_SEARCH
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.SearchView
 import android.view.Menu
-import android.widget.SearchView
+import android.view.View
 import com.ngallazzi.watchersexplorer.remote.GithubApi.Companion.disposable
 import com.ngallazzi.watchersexplorer.remote.GithubApi.Companion.gitHubApiServe
 import com.ngallazzi.watchersexplorer.remote.PaginatedResponse
+import com.ngallazzi.watchersexplorer.remote.models.Repository
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_search_repositories.*
 
 
 class SearchRepositoriesActivity : AppCompatActivity() {
+
+    var repositories: ArrayList<Repository> = ArrayList()
+    lateinit var query: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +32,11 @@ class SearchRepositoriesActivity : AppCompatActivity() {
         if (ACTION_SEARCH == intent.action) {
             intent.getStringExtra(SearchManager.QUERY)?.also { query ->
                 searchRepositories(query)
+                this.query = query
             }
         }
+        rvRepositories.layoutManager = LinearLayoutManager(this)
+        rvRepositories.adapter = RepositoryAdapter(repositories, this)
     }
 
     fun searchRepositories(key: String) {
@@ -37,21 +44,34 @@ class SearchRepositoriesActivity : AppCompatActivity() {
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
-                        { result -> showRepos(result) },
+                        { result ->
+                            when {
+                                result.itemsCount > 0 -> {
+                                    rvRepositories.visibility = View.VISIBLE
+                                    tvHint.visibility = View.GONE
+                                    showRepos(result)
+                                }
+                                else -> {
+                                    tvHint.visibility = View.VISIBLE
+                                    rvRepositories.visibility = View.GONE
+                                    showError(error = getString(R.string.no_repositories_found))
+                                }
+                            }
+                        },
                         { error -> showError(error = error.message!!) }
                 )
     }
 
-    fun showRepos(response: PaginatedResponse) {
-        for (repo in response.items) {
-            Log.v(TAG, "Repository: " + repo.name)
+    private fun showRepos(response: PaginatedResponse) {
+        repositories.clear()
+        for (item in response.items) {
+            repositories.add(item)
         }
     }
 
-    fun showError(error: String) {
+    private fun showError(error: String) {
         Snackbar.make(clContainer, error, duration)
-                .setAction(getString(R.string.retry),
-                        { searchRepositories(KEY) })
+                .setAction(getString(R.string.retry)) { searchRepositories(query) }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -60,6 +80,8 @@ class SearchRepositoriesActivity : AppCompatActivity() {
         val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
         (menu.findItem(R.id.search).actionView as SearchView).apply {
             setSearchableInfo(searchManager.getSearchableInfo(componentName))
+            setIconifiedByDefault(false)
+            requestFocusFromTouch()
         }
 
         return true
@@ -67,6 +89,5 @@ class SearchRepositoriesActivity : AppCompatActivity() {
 
     companion object {
         val TAG = SearchRepositoriesActivity::class.simpleName
-        val KEY = "tripbook"
     }
 }
